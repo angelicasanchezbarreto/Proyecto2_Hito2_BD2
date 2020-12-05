d586c936b64345655e7f996e9b2a0a12062f3d13
Alejandro  Otero <alejandrotero@Alejandros-MacBook-Air.local>
2020-12-05 11:47:00 -0500

    Range search updated

--- rangeRtree.py
+++ rangeRtree.py
@@ -3,16 +3,21 @@ class  Node:
     mbrs = [] #mbsr of children 
 
 def searchRange(node, Q, r):
+    listResult = []
     if node.isLeaf():
         for C in node:
             d = ED(C, Q)    
             if d <= r:  #refine
-                listResult.add( (d, C) )
+                listResult.append( (d, C) )
     else:
         for i in range(len(node.children)):            
             if MINDIST(Q, node.mbrs[i] ) <= r: #filter
                 child = readNode(node.children[i])
                 searchRange(child, Q, r)
 ##
-listResult = []
-searchRange(root, Q, r, listResult);        
+
+searchRange(root, Q, r)
+
+
+
+

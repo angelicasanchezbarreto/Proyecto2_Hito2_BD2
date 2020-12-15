8e2a2338562119fd80bf95f5b3f0b5d202320a2b
angelicasanchezbarreto <angelica.sanchez@utec.edu.pe>
2020-12-15 06:14:30 -0500

    adding files

--- knnSearch.py
+++ knnSearch.py
@@ -15,15 +15,15 @@ def knnSequential(Query,k,data):
     return heap_max.get_k_elem(k)
 
 def knnRtree(Query,k,data):
-    """ if path.exists('rtree_index.data'):
+    if path.exists('rtree_index.data'):
         remove('rtree_index.data')
     if path.exists('rtree_index.index'):
-        remove('rtree_index.index') """
+        remove('rtree_index.index')
     p = index.Property()
     p.dimension = 256
     p.buffering_capacity = 23
-    """ p.dat_extension = 'data'
-    p.idx_extension = 'index' """
+    p.dat_extension = 'data'
+    p.idx_extension = 'index'
     idx = index.Index('rtree_index',properties=p)
     result = []
     id=0

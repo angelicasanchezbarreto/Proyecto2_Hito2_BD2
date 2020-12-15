45d00f3e97baa197a6295ba0cc087f5ad59b7bd1
Alejandro  Otero <alejandrotero@Alejandros-MacBook-Air.local>
2020-12-14 20:21:18 -0500

    Frontend finished

--- fronted/views.py
+++ fronted/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
 from django.template import Template, Context
 from django.template.loader import get_template
+from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
+#from console import Console
 
 def face_recognition(request):
-    #tweets = request.GET["tweet"]
-    #dic = Console()
+    if request.method == 'POST':
+        Image = request.FILES["image"]
+        fs = FileSystemStorage()
+        fs.save(Image.name, Image)
+        Algorithm = request.POST["algorithm"]
+        neighbors = request.POST["num"]
     #answer = dic.print_in_console(tweets)
-    return render(request, 'face_recognition.html', {"Tweets": "hola"})
+    names = ['Johnny Dep']
+    return render(request, 'face_recognition.html', {"Artists": names})
